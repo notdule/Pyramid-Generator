a871f65a5e269a064d893f4bd0bf5c7dca6fb2f2
notdule <148584497+notdule@users.noreply.github.com>
2024-04-21 23:40:33 +0200

    Basic code change

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 let character = 'Hello';
-console.log(character);
-character = "World";
-let secondCharacter;
-secondCharacter = "Test";
-console.log(secondCharacter);
+let count = 8;
+let rows = [];
