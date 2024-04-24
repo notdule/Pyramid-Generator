718f940bcb9a09b5a308524c6e8cd5001a8f43ac
notdule <148584497+notdule@users.noreply.github.com>
2024-04-24 23:07:34 +0200

    For loop lection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push("freeCodeCamp");
-
-console.log(rows);
+const character = "#";
+const count = 8;
+const rows = [];
+for (let i = 0; i < count; i = i + 1) {
+  console.log(i);
+}
