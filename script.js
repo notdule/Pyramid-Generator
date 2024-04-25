deb7a99fc0b0501f032e87e2b05dd7bd075de469
notdule <148584497+notdule@users.noreply.github.com>
2024-04-25 23:42:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const character = "#";
 const count = 8;
 const rows = [];
 for (let i = 0; i < count; i = i + 1) {
-  console.log(i);
+  rows.push(character.repeat(i))
 }
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
