3d82f6e0537750ca3bbcd4dc416ec752cf0ee19d
notdule <148584497+notdule@users.noreply.github.com>
2024-04-26 23:14:44 +0200

    padRow function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const character = "#";
 const count = 8;
 const rows = [];
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i))
+
+function padRow() {
+
 }
 
-let result = "";
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
 
 for (const row of rows) {
   result = result + "\n" + row;
