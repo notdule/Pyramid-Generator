7cd791c89e1aafe7f9de1d8a6c92d124be86aa01
notdule <148584497+notdule@users.noreply.github.com>
2024-04-29 11:50:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i += 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (false) {
+  console.log("Condition is true");
 }
 
-let result = ""
+let result = "";
 
 for (const row of rows) {
   result = result + "\n" + row;
